01d05ba31e8473b09c83d631a6b49d1e8dd41042
Aline Diniz <116644964+alidiniz@users.noreply.github.com>
2023-06-03 15:57:15 +0000

    Cria classes e add atributos e métodos

---
+++ InstrumentoFinanceiro.java
@@ -0,0 +1,15 @@
+public class InstrumentoFinanceiro {
+	private float saldo;
+	
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public float calcularSaldoTotal() {
+        return saldo;
+    }
+}
